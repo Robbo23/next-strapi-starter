5674f18a48d65160191928d8a2abef052283b20a
robbo23 <robbo1995@gmail.com>
2022-03-06 14:07:21 +0100

    some test changes on backend

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,7 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'e6eb25fd8433ef7263b3436fec672a85')
-  },
-  serveAdminPanel: env.bool('SERVE_ADMIN', true),
-  url: env('PUBLIC_ADMIN_URL', '/admin')
+  }
 })
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://panel.server829206.nazwa.pl')
+  port: env.int('PORT', 8000),
+  app: {
+    keys: env.array('APP_KEYS')
+  }
 })
 
