d7def11f2bffe972416adc3504bafda99a7edd79
robbo23 <robbo1995@gmail.com>
2022-03-06 14:27:15 +0100

    some test changes on backend

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,5 +1,7 @@
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'e6eb25fd8433ef7263b3436fec672a85')
-  }
+  },
+  serveAdminPanel: env.bool('SERVE_ADMIN', true),
+  url: env('PUBLIC_ADMIN_URL', '/admin'),
 })
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,8 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8000),
-  app: {
-    keys: env.array('APP_KEYS')
-  }
+  url: env('PUBLIC_URL', 'http://panel.server829206.nazwa.pl'),
+  proxy: env.bool('IS_PROXIED', true),
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false),
+  },
+  
 })
 
