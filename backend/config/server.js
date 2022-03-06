ef63c53c5f7772faf90a0a40e721abde96edfeb3
robbo23 <robbo1995@gmail.com>
2022-03-06 15:28:07 +0100

    some test changes on backend

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,11 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8000),
-  url: env('PUBLIC_URL', 'http://panel.server829206.nazwa.pl'),
-  proxy: env.bool('IS_PROXIED', true),
-  cron: {
-    enabled: env.bool('CRON_ENABLED', false),
-  },
+  // proxy: env.bool('IS_PROXIED', true),
+  // cron: {
+  //   enabled: env.bool('CRON_ENABLED', false),
+  // },
   
 })
 
