f83921322e1a737d33b050f4be863f49b43f1a80
Robbo23 <47586046+Robbo23@users.noreply.github.com>
2022-03-09 15:03:50 +0100

    Update server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,10 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8000),
-  // proxy: env.bool('IS_PROXIED', true),
-  // cron: {
-  //   enabled: env.bool('CRON_ENABLED', false),
-  // },
-  
+  url: 'http://next-test.server924660.nazwa.pl/panel'
 })
 
